e02bd281d0b0c4c7c7a5a44b0e797f4bd36b2e30
kmarks2013 <kmarks2013@gmail.com>
2021-02-27 15:19:36 -0500

    removes user and views from params because they will be set elsewehre

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -66,6 +66,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :body, :user, :views)
+      params.require(:post).permit(:title, :body)
     end
 end
