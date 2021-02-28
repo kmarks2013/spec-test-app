69525cbb46523794d0ba1bdf71da2f8e170043e6
kmarks2013 <kmarks2013@gmail.com>
2021-02-27 22:21:06 -0500

    adds logic for the hundred character length

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -62,6 +62,11 @@ RSpec.describe Post, type: :model do
 
     post.body = '12345'
     expect(post).to be_valid
+
+    hundred_char_string = '2YaRjEbx9oXSxPpMwrI51GEdBseuJDrbFzD1XOxIhdyIPkoqlIgrpAekSFdAJdkbdZRIwKWZ4Ia8YGWAmTzTFvqQ7ysdk02lZ6GQ
+    '
+    post.body = hundred_char_string
+    expect(post).to be_valid
   end
 
   it ' has numerical views' do
