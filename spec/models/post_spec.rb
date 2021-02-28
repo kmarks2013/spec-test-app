77ac72bdf281d9b309aae9f33c4d1dbcc45b06a5
kmarks2013 <kmarks2013@gmail.com>
2021-02-27 20:14:09 -0500

    adds comments explaining how the users work in these tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # create a current user for this test.  it will not have a session but it will assume a current user already exists
+  #  the tests in this page will assume that you have a current user to even get tothis page 
+
+  # if you wanted to check that there was an actual current_user add that in your before action in the controller
+  # example  in the POSTS controller:
+      #  before_action authenticate_user!
+  # another test will validate that there is a current user before you get to this page.
+
 end
