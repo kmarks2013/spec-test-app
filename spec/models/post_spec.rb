28033e9e3467983cd8628013e0eb49cc011c2981
kmarks2013 <kmarks2013@gmail.com>
2021-02-27 20:15:05 -0500

    removes whitespace and creates user for testing

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   # create a current user for this test.  it will not have a session but it will assume a current user already exists
-  #  the tests in this page will assume that you have a current user to even get tothis page 
-
+  #  the tests in this page will assume that you have a current user to even get tothis page
   # if you wanted to check that there was an actual current_user add that in your before action in the controller
   # example  in the POSTS controller:
       #  before_action authenticate_user!
   # another test will validate that there is a current user before you get to this page.
 
+  current_user = User.find_or_create!(email: 'testcase@example.com', password: 'password', password_confirmation: 'password')
 end
