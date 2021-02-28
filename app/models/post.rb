947c514a99e16bfa152c00d8e9a891c8f2ae3eef
kmarks2013 <kmarks2013@gmail.com>
2021-02-27 19:54:12 -0500

    updates numericality validaiton

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
     belongs_to :user
     validates :title, presence: true, length: {minimum: 2}
     validates :body, presence: true, length: {in: 5..100}
-    validates :views, numericality
+    validates :views, numericality: { only_integer: true }
 end
